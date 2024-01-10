699fd375a5ad2e2d82652982e13de0590ab0a630
Nelson Rosario <NellyisDev@gmail.com>
2024-01-09 19:55:21 -0500

    Added my project

---
+++ themes/Press813/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ar">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <style>
+    :lang(ar) {
+      direction: rtl;
+    }
+  </style>
+  <body>
+    <h1>Hello how are you doing today!</h1>
+  </body>
+</html>
